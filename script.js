9ee0067622d85a8f28c1be6501003bec972cc9c7
MOHD SAKIB <105626034+19SAKIB@users.noreply.github.com>
2024-11-19 21:09:01 +0530

    Update script.js
    some changes

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function showEmotion() {
     emotions.classList.remove("hidden");
     let message = document.getElementById("message");
     message.textContent = "I truly care about you and I'm so sorry. 💖 Please forgive me!";
-    let getName = document.getElementById("name");
-    getName.textContent = "💖💖💖Sorry! Fatima 💖💖💖";
+    let name = document.getElementById("name");
+    name.textContent = "💖💖💖 Sorry! Fatima  💖💖💖";
+     
 }
  
